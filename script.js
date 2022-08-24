770258ea8bf783e2a4fcfca79b9971b8f305a033
Saurav Kumar Verma <sauravkumarverma22@gmail.com>
2022-08-24 17:38:55 +0530

    Working Buttons addedEvent listeners

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
 let quoteList = [];
 
 const randomQuote = () => {
   const singleQuote = quoteList[Math.floor(Math.random() * quoteList.length)];
-  console.log(singleQuote);
+
+  //   Check if Author Field is blank and fill it with Unknown
+
+  singleQuote.author
+    ? (authorText.textContent = singleQuote.author)
+    : (authorText.textContent = 'Unknown');
+
+  // Check quote length to determine styling.
+
+  singleQuote.text.length > 120
+    ? quoteText.classList.add('long-quote')
+    : quoteText.classList.remove('long-quote');
+
+  quoteText.textContent = singleQuote.text;
 };
 
 async function getQuotes() {
@@ -15,4 +34,15 @@ async function getQuotes() {
   } catch (error) {}
 }
 
+//  Tweet Quote
+
+const tweetQuote = () => {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+
+  window.open(twitterUrl, '_blank');
+};
+
+// Adding Event Listeners
+newQuoteBtn.addEventListener('click', randomQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 getQuotes();
